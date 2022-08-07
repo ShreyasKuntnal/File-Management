b1acd4dd831efa3914ee32b3d5ecc6c600e574be
Shreyas K <shreyas.kuntnal125@gmail.com>
2022-08-07 18:27:57 +0530

    menu bar

--- src/components/google-drive/Dashboard.js
+++ src/components/google-drive/Dashboard.js
@@ -9,6 +9,55 @@ import Navbar from "./Navbar"
 import FolderBreadcrumbs from "./FolderBreadcrumbs"
 import { useParams, useLocation } from "react-router-dom"
 import { useAuth } from "../../contexts/AuthContext"
+// import { Menu, , MenuButton, SubMenu} from "@szhsin/react-menu";
+import "../../CSS/menu_css.css";
+import { ContextMenu,MenuItem, ContextMenuTrigger } from "react-contextmenu";
+import { render } from 'react-dom'
+
+import { getStorage, ref, deleteObject } from "firebase/storage";
+
+const storage = getStorage();
+
+// // Create a reference to the file to delete
+const desertRef = ref(storage, 'images/desert.jpg');
+
+// // Delete the file
+// deleteObject(desertRef).then(() => {
+//   // File deleted successfully
+// }).catch((error) => {
+//   // Uh-oh, an error occurred!
+// });
+
+// let imageRef = storage.refFromURL(URL);
+// imageRef.delete()
+
+const deleteFromFirebase = (url) => {
+  //1.
+  let pictureRef = storage.refFromURL(url);
+ //2.
+  pictureRef.delete()
+    // .then(() => {
+    //   //3.
+    //   setImages(allImages.filter((image) => image !== url));
+    //   alert("Picture is deleted successfully!");
+    // })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const ID = 'ID'
+
+const handleClick = (event, data) => {
+  console.log(`clicked`, { event, data })
+}
+
+const attributes = {
+  className: 'custom-root',
+  disabledClassName: 'custom-disabled',
+  dividerClassName: 'custom-divider',
+  selectedClassName: 'custom-selected'
+}
 
 export default function Dashboard() {
   const { folderId } = useParams()
@@ -33,7 +82,9 @@ export default function Dashboard() {
                 style={{ maxWidth: "250px" }}
                 className="p-2"
               >
+                <ContextMenuTrigger id={ID}>
                 <Folder folder={childFolder} />
+            </ContextMenuTrigger>  
               </div>
             ))}
           </div>
@@ -46,8 +97,38 @@ export default function Dashboard() {
                 key={childFile.id}
                 style={{ maxWidth: "250px" }}
                 className="p-2"
-              >
-                <File file={childFile} />
+              > 
+              <ContextMenuTrigger id={ID}>
+              <File file={childFile} />
+            </ContextMenuTrigger>
+            <ContextMenu id={ID}>
+            <MenuItem
+              data={{ action: 'Open' }}
+              onClick={handleClick}
+              attributes={attributes}
+            >
+              Open
+            </MenuItem>
+            <MenuItem
+              data={{ action: 'Rename' }}
+              onClick={handleClick}
+              attributes={attributes}
+            >
+              Rename
+            </MenuItem>
+            <MenuItem divider />
+            <MenuItem
+              data={{ action: 'Delete' }}
+              onClick={handleClick}
+              attributes={attributes}
+            >
+              {/* <button onClick={() => deleteFromFirebase(childFile)}>
+               Delete
+              </button> */}
+              Delete
+            </MenuItem>
+            </ContextMenu>
+
               </div>
             ))}
           </div>
